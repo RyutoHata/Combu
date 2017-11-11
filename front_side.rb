b4f8e0a5c906069b0e58a300d301c3e14f56afa9
Ryuto Hata <ryuto.aiit@gmail.com>
2017-11-11 10:23:17 +0900

    エンドポイントからマネージャを呼び出す

--- front_side.rb
+++ front_side.rb
@@ -13,34 +13,39 @@ post '/vm' do
 
   # 受け取ったparamsをjsonに突っ込む
   start = set_start_params
-  puts start
 
-  # マネジャーを呼び出す
-  #dbmgr = AccessLibrary.new
-  #dbmgr.request(start)
+  call_manager(start)
   erb :show
 end
 
 get '/vm' do
   list = set_list_params
-  puts list
-
+  call_manager(list)
 end
 
 put '/vm/:id' do
-  "vm #{params[:id]} power_control"
+  power = set_power_params
+  call_manager(power)
 end
 
 delete '/vm/:id' do
-  "delete vm #{params[:id]}"
+  terminated = get_terminated_params
+  call_manager(terminated)
 end
 
 delete '/vm' do
   all_terminated = set_all_terminated_params
-  puts all_terminated
+  call_manager(all_terminated)
 end
 
 private
+
+def call_manager(command)
+  # マネジャーを呼び出す
+  #dbmgr = AccessLibrary.new
+  #dbmgr.request(command)
+end
+
 def set_start_params
   request = {}
   request["Req_id"] = 1
@@ -56,6 +61,24 @@ def set_list_params
   request["Command"] = "list"
   list = JSON.dump(request)
   return list
+end
+
+def set_power_params
+  request = {}
+  request["Req_id"] = 1
+  request["Command"] = params[:power]
+  request["VM_id"] = params[:id]
+  power = JSON.dump(request)
+  return power
+end
+
+def set_terminated_params
+  request = {}
+  request["Req_id"] = 1;
+  request["Command"] = "terminate"
+  request["VM_id"] = params[:id]
+  terminated = JSON.dump(request)
+  return terminated
 end
 
 def set_all_terminated_params
