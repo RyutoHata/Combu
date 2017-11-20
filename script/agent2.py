889636347bd87cb33cdb15e3a365fd731cefebb3
kmiyazaki3 <tktrnoknlw@gmail.com>
2017-11-20 18:59:03 +0900

    Update agent2.py

--- script/agent2.py
+++ script/agent2.py
@@ -2,21 +2,26 @@
 #coding: UTF-8
 
 import os
+import time
 
 #定義--------------------
 #鯖名のフォルダ
-SABA_PATH = "IA02/"
-WRITE_PATH = "/var/ia01/recv/"
-READ_PATH = "/mnt/ia01/send/"
+#@@@sabaname
+SABA_PATH = "@@@/"
+READ_PATH = "/mnt/ia01/combu/send/"
 
 while 1:
     #フォルダ内読み込み
-    file_list = os.listdir(READ_PATH)
-    #ファイル
-    f = open(READ_PATH + file_list[0],"r")
-    #読み込み
-    data = f.read()
-    f.close()
-    #実行
-    c = os.system("./start.sh " + data)
-    os.remove(READ_PATH + file_list[0])    #削除
+    file_list = os.listdir(READ_PATH + SABA_PATH)
+    if len(file_list) != 0:
+        #ファイル
+        f = open(READ_PATH + SABA_PATH + file_list[0],"r")
+        #読み込み
+        data = f.read()
+        f.close()
+        #実行
+        c = os.system(data)
+        os.remove(READ_PATH + SABA_PATH + file_list[0])    #削除
+    else:
+        time.sleep(2)
+        
